1b68a26eea88e4f93a3ee4bdf03f99291d1756f3
ruther60 <ruther60@UTORONTO.CA>
2016-11-14 23:19:20 +0000

    working on itinerary.java

--- p2/CSCB07Project/src/Itinerary/Itinerary.java
+++ p2/CSCB07Project/src/Itinerary/Itinerary.java
@@ -1,5 +1,35 @@
 package Itinerary;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import Flight.Flight;
+
+
 public class Itinerary {
 
+	private List<Flight> flights = new ArrayList<Flight>();
+	private double totalCost = 0;
+	private double totalTime = 0;
+	
+	public Itinerary() {
+		
+	}
+	
+	public boolean addFlight(Flight flight) {
+		
+		totalCost += flight.getCost();
+		
+		return flights.add(flight);
+		
+	}
+	
+	public boolean removeFlight(Flight flight) {
+		
+		totalCost -= flight.getCost();
+		
+		return flights.remove(flight);
+		
+	}
+	
 }
