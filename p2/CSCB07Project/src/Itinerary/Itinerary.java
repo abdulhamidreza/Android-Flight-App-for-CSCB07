75f66b63a4b573b137873e907d6efcd911ea7553
ruther60 <ruther60@UTORONTO.CA>
2016-11-15 06:10:24 +0000

    still working on itinerary.java

--- p2/CSCB07Project/src/Itinerary/Itinerary.java
+++ p2/CSCB07Project/src/Itinerary/Itinerary.java
@@ -9,8 +9,8 @@ import Flight.Flight;
 public class Itinerary {
 
 	private List<Flight> flights = new ArrayList<Flight>();
-	private double totalCost = 0;
-	private double totalTime = 0;
+	private double totalCost = 0; //the total cost in $
+	private long totalTime = 0; //the total time in milliseconds
 	
 	public Itinerary() {
 		
@@ -18,7 +18,10 @@ public class Itinerary {
 	
 	public boolean addFlight(Flight flight) {
 		
-		totalCost += flight.getCost();
+		totalCost += flight.getCost();	
+		//the total time is the time from the beginning of the first flight to the end of the last flight
+		
+		totalTime = flight.getDepartureDate().getTimeInMillis() - flights.get(0).getArrivalDate().getTimeInMillis();
 		
 		return flights.add(flight);
 		
@@ -28,8 +31,69 @@ public class Itinerary {
 		
 		totalCost -= flight.getCost();
 		
-		return flights.remove(flight);
+		boolean canRemove = flights.remove(flight);
 		
+		//the total time is the time from the beginning of the first flight to the end of the second last flight (after the last is removed)
+		totalTime = flights.get(flights.size()-1).getDepartureDate().getTimeInMillis() - flights.get(0).getArrivalDate().getTimeInMillis();
+		
+		return canRemove;
+		
+	}
+	
+	public int getItineraryId(){
+		
+		return this.hashCode();
+		
+	}
+ 
+	public double getTotalCost() {
+		return totalCost;
+	}
+
+	public void setTotalCost(double totalCost) {
+		this.totalCost = totalCost;
+	}
+
+	public long getTotalTime() {
+		return totalTime;
+	}
+
+	public void setTotalTime(long totalTime) {
+		this.totalTime = totalTime;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((flights == null) ? 0 : flights.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(totalCost);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + (int) (totalTime ^ (totalTime >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Itinerary other = (Itinerary) obj;
+		if (flights == null) {
+			if (other.flights != null)
+				return false;
+		} else if (!flights.equals(other.flights))
+			return false;
+		if (Double.doubleToLongBits(totalCost) != Double
+				.doubleToLongBits(other.totalCost))
+			return false;
+		if (totalTime != other.totalTime)
+			return false;
+		return true;
 	}
 	
 }
