a24f117adfd3f0d0013a8a38124d9801b2cf6f0d
Stephen Luc <stephenluc@users.noreply.github.com>
2016-11-24 21:48:43 +0000

    Created admin activity

---
+++ p3a/CSCB07CourseProject/app/src/main/java/cs/b07/cscb07courseproject/AdminActivity.java
@@ -0,0 +1,26 @@
+package cs.b07.cscb07courseproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import cs.b07.cscb07courseproject.users.Admin;
+
+public class AdminActivity extends AppCompatActivity {
+
+    private EditText detailBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+
+        Intent intent = getIntent();
+
+        Admin admin = (Admin) intent.getSerializableExtra("Admin");
+
+        detailBox = (EditText) findViewById(R.id.adminDetailEt);
+        detailBox.setText(admin.toString());
+    }
+}
